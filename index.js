b3eea25643fdd05b64f3d1ac518f1dc5a52ff8b1
ThreeSnakes <dev.select995@gmail.com>
2019-06-06 02:33:14 +0900

    feat: add corn func && install rquest module
    - install node-cron && cron func
    - install rqeust && request-promise

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+const cron = require('node-cron');
+
+// const executionCycle = "0 50 23 * * *";
+const executionCycle = "*/30 * * * * *";
+
 console.info("HELLO AUTOMATIC_HABIT_RECORDER!!!!");
+
+if (!cron.validate(executionCycle)) {
+  console.error("[ERROR] CRON CYCLE IS NOT CORRECT. CHECK VALIDATION");
+  process.exit(true);
+}
+
+const task = cron.schedule(executionCycle, () => {
+  console.info("[INFO] RUN AUTOMATIC_HABIT_RECORDER. ");
+}, {
+  schedule: false
+});
+
+task.start();
 
 module.exports = {
   test: () => {
--- test/index.spec.js
+++
@@ -1,9 +0,0 @@
-const index = require('../index');
-
-describe("index.js TEST", () => {
-  it("TEST function must return 'TEST'", (done) => {
-    if (index.test() === 'TEST') {
-      done();
-    }
-  });
-});
