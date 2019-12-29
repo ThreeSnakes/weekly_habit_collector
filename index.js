9f168b275bf9272097e0bbf00f402b385db6e079
ThreeSnakes <dev.select995@gmail.com>
2019-12-29 21:41:29 +0900

    ADD: 메일 Body Parsing 기능

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const cron = require('node-cron');
+const _ = require('lodash');
 
-const twitter_lib = require('./lib/twiiter');
-const mail_lib = require('./lib/mail');
+const { filter, getMyTwitters } = require('./lib/twiiter');
+const { classifyTweetContent } = require('./lib/util');
+const { sendMail, makeMailBody } = require('./lib/mail');
 
 // const executionCycle = "0 50 23 * * *";
 const executionCycle = "*/30 * * * * *";
@@ -15,10 +17,8 @@ if (!cron.validate(executionCycle)) {
 
 const task = cron.schedule(executionCycle, async () => {
   console.info("[INFO] RUN AUTOMATIC_HABIT_RECORDER. ");
-  const tweets = await twitter_lib.getMyTwitters();
-  const filteredTweets = twitter_lib.filter(tweets);
-  console.info(JSON.stringify(filteredTweets, null, 4));
-  await mail_lib.sendMail("TEST");
+  await sendMail(makeMailBody(classifyTweetContent(filter(await getMyTwitters()))));
+  process.exit(1);
 }, {
   schedule: false
 });
