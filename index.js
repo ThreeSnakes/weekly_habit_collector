a65b24a2f521584de3993afbac9a8cb01c9c53e4
ThreeSnakes <dev.select995@gmail.com>
2019-12-29 21:44:28 +0900

    DELETE: cron 기능 삭제

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-const cron = require('node-cron');
-const _ = require('lodash');
-
 const { filter, getMyTwitters } = require('./lib/twiiter');
 const { classifyTweetContent } = require('./lib/util');
 const { sendMail, makeMailBody } = require('./lib/mail');
 
-// const executionCycle = "0 50 23 * * *";
-const executionCycle = "*/30 * * * * *";
-
-console.info("HELLO AUTOMATIC_HABIT_RECORDER!!!!");
-
-if (!cron.validate(executionCycle)) {
-  console.error("[ERROR] CRON CYCLE IS NOT CORRECT. CHECK VALIDATION");
-  process.exit(true);
-}
-
-const task = cron.schedule(executionCycle, async () => {
-  console.info("[INFO] RUN AUTOMATIC_HABIT_RECORDER. ");
+const runner = async () => {
+  console.info("[INFO] RUN AUTOMATIC_HABIT_RECORDER.");
   await sendMail(makeMailBody(classifyTweetContent(filter(await getMyTwitters()))));
+  console.info("[INFO] FINISH AUTOMATIC_HABIT_RECORDER.");
   process.exit(1);
-}, {
-  schedule: false
-});
-
-task.start();
-
-module.exports = {
-  test: () => {
-    return "TEST";
-  }
 };
+
+runner();
