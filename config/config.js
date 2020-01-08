e8c9c9c9cd90250098a083cfcfe7641d1901f5c1
ThreeSnakes <dev.select995@gmail.com>
2020-01-09 00:51:34 +0900

    ADD: nodemailer google 연동을 OAuth2로 변경
    - user, pwd로 하는 경우 github Action을 실행할때 네덜란드, 미국 버지니아
    등 여러 나라에서 수행되다보니 google에서 차단 시켜버리는 경우가 발생.
    그래서 이를 막기 위해 OAuth2로 동작하도록 변경 하였다.

--- config/config.js
+++ config/config.js
@@ -8,9 +8,12 @@ const config = {
     consumer_key: process.env.TWITTER_API_KEY || _.get(dev_config, 'twitter.consumer_key'),
     consumer_secret: process.env.TWITTER_API_SECRET_KEY || _.get(dev_config, 'twitter.consumer_secret')
   },
-  mail: {
-    user: process.env.GMAIL_USER || _.get(dev_config, 'mail.user'),
-    pwd: process.env.GMAIL_PWD || _.get(dev_config, 'mail.pwd')
+  google: {
+    user: process.env.GOOGLE_USER || _.get(dev_config, 'google.user'),
+    client_id: process.env.GOOGLE_CLIENT_ID || _.get(dev_config, 'google.client_id'),
+    client_secret: process.env.GOOGLE_CLIENT_SECRET || _.get(dev_config, 'google.client_secret'),
+    refresh_token: process.env.GOOGLE_REFRESH_TOKEN || _.get(dev_config, 'google.refresh_token'),
+    redirect_url: "https://developers.google.com/oauthplayground"
   }
 };
 
--- test/mail.spec.js
+++ test/mail.spec.js
@@ -5,5 +5,5 @@ const mail_lib = require('../lib/mail');
 describe('TEST Mail Module.', () => {
   it("test sendMail()", (done) => {
     mail_lib.sendMail("TEST").then(() => done());
-  }).timeout(5000);
+  }).timeout(10 * 1000);
 });
