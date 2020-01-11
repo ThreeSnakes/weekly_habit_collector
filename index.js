d806fb299e0f9946a78f6d2b5d3046fb08107d12
ThreeSnakes <dev.select995@gmail.com>
2020-01-11 22:45:39 +0900

    UPDATE: 메일 포맷 수정
    - 메일 전송시 태그별이 아닌 날짜별로 정렬되도록 수정.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { filter, getMyTwitters } = require('./lib/twiiter');
-const { classifyTweetContent } = require('./lib/util');
+const { classifyTweetContentByCreatedAt } = require('./lib/util');
 const { sendMail, makeMailBody } = require('./lib/mail');
 
 const runner = async () => {
   console.info("[INFO] RUN AUTOMATIC_HABIT_RECORDER.");
-  await sendMail(makeMailBody(classifyTweetContent(filter(await getMyTwitters()))));
+  await sendMail(makeMailBody(classifyTweetContentByCreatedAt(filter(await getMyTwitters()))));
   console.info("[INFO] FINISH AUTOMATIC_HABIT_RECORDER.");
   process.exit(0);
 };
